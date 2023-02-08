1325fe2c43e69e324e964d8ca39c0e1e6ea4816b
David Sehi <david.sehi2016@gmail.com>
2023-02-08 12:05:29 +0000

    Add status property and update CSS

--- src/pages/Home/UI/TodoGrid/Todo.jsx
+++ src/pages/Home/UI/TodoGrid/Todo.jsx
@@ -1,11 +1,12 @@
 import { IoTrash } from 'react-icons/io5'
 import { dayjs } from '../../../../lib/dayjs'
 import { useStore } from '../../../../store/storeContext'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-hot-toast'
 
 function Todo(todoObj) {
-  const { todoId, todo, updatedAt } = todoObj
+  const { todoId, todo, updatedAt, isImportant } = todoObj
   const time = dayjs(updatedAt).fromNow()
   const store = useStore()
   const navigate = useNavigate()
@@ -15,6 +16,7 @@ function Todo(todoObj) {
     userId,
     todoId,
     todo,
+    isImportant,
   }
 
   const goToUpdate = () => {
@@ -51,12 +53,22 @@ function Todo(todoObj) {
 
   return (
     <div
-      className="relative z-0 flex w-full cursor-default items-center justify-between gap-1 rounded-lg border border-gray-200 bg-white p-4 transition hover:bg-gray-200/70"
+      className={`relative z-0 flex w-full cursor-pointer items-center justify-between gap-1 rounded-lg ${
+        isImportant
+          ? 'border-none bg-[#ffc300] hover:bg-[#ffb700]'
+          : 'border border-gray-200 bg-white hover:bg-gray-200/70'
+      } p-4 transition`}
       onClick={goToUpdate}
     >
       <div className="flex h-full w-full flex-col justify-between gap-2">
         <p>{todo}</p>
-        <span className="text-sm font-medium text-slate-500">{time}</span>
+        <span
+          className={`text-sm font-medium ${
+            isImportant ? 'text-back' : 'text-slate-500'
+          }`}
+        >
+          {time}
+        </span>
       </div>
       <button
         className="relative z-[999] inline-flex items-center rounded-full bg-red-700 p-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300"
